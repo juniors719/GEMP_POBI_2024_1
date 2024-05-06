699182ae3f44fbb5c27913d87ee55cd73c961df3
Júnior Silva <junior.silva@alu.ufc.br>
2024-05-06 12:54:29 -0300

    #

--- 06-Stack/problems/zero_pra_cancelar/zero.cpp
+++ 06-Stack/problems/zero_pra_cancelar/zero.cpp
@@ -2,3 +2,21 @@
 #include <stack>
 
 using namespace std;
+
+int main() {
+    int n, soma{}, leitura;
+    stack<int> sequencia;
+    cin >> n;
+    while (n--) {
+        cin >> leitura;
+        if (leitura)
+            sequencia.push(leitura);
+        else if (!leitura && !sequencia.empty())
+            sequencia.pop();
+    }
+    while (!sequencia.empty()) {
+        soma += sequencia.top();
+        sequencia.pop();
+    }
+    cout << soma << endl;
+}
